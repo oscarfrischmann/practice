4f23d92053e76b6da7acd4d146fdb05c7e1b512b
Oscar Frischmann <oscarfrisc@gmail.com>
2024-06-26 23:14:54 -0300

    profile icon

--- mflix/src/routes/views.routes.js
+++ mflix/src/routes/views.routes.js
@@ -1,91 +1,104 @@
-import { Router } from 'express';
-import moviesModel from '../dao/models/movies.model.js';
-import MovieManager from '../dao/manager.mdb.js';
-import countriesModel from '../dao/models/countries.model.js';
-import fs from 'fs';
-import config from '../config.js';
+import { Router } from "express";
+import moviesModel from "../dao/models/movies.model.js";
+import MovieManager from "../dao/manager.mdb.js";
+import countriesModel from "../dao/models/countries.model.js";
+import fs from "fs";
+import config from "../config.js";
 
 const router = Router();
 const manager = new MovieManager();
 const login = async (req, res, next) => {
-	if (req.session.user) {
-		next();
-	} else {
-		return res.redirect('/');
-	}
+  if (req.session.user) {
+    next();
+  } else {
+    return res.redirect("/");
+  }
 };
-router.get('/', async (req, res) => {
-	console.log(req.session.user);
-	try {
-		if (req.query.error) {
-			res.render('login', { origin: 'login', error: true });
-		} else {
-			res.render('login', { origin: 'login' });
-		}
-	} catch (err) {
-		throw new Error(err);
-	}
+router.get("/", async (req, res) => {
+  console.log(req.session.user);
+  try {
+    if (req.query.error) {
+      res.render("login", { origin: "login", error: true });
+    } else {
+      res.render("login", { origin: "login" });
+    }
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
-router.get('/mov', login, async (req, res) => {
-	let currentPage;
-	if (req.query) {
-		currentPage = req.query;
-	} else {
-		currentPage = '1';
-	}
-	try {
-		console.log(req.session.user);
-		// if (req.session.user) {
-		const countries = await countriesModel.findOne({}).lean();
-		const data = await moviesModel.paginate({}, { limit: 8, lean: true, page: +currentPage.page });
-		res.render('movies', { movies: data, countries: countries, user: req.session.user });
-		// } else {
-		// 	res.redirect('/');
-		// }
-	} catch (err) {
-		throw new Error(err);
-	}
+router.get("/mov", login, async (req, res) => {
+  let currentPage;
+  if (req.query) {
+    currentPage = req.query;
+  } else {
+    currentPage = "1";
+  }
+  try {
+    console.log(req.session.user);
+    // if (req.session.user) {
+    const countries = await countriesModel.findOne({}).lean();
+    const data = await moviesModel.paginate(
+      {},
+      { limit: 8, lean: true, page: +currentPage.page }
+    );
+    res.render("movies", {
+      movies: data,
+      countries: countries,
+      user: req.session.user,
+    });
+    // } else {
+    // 	res.redirect('/');
+    // }
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
-router.get('/mov/one/:movid', async (req, res) => {
-	try {
-		const data = await moviesModel.findOne({ _id: req.params.movid }).lean();
-		const countries = await manager.getCountries();
-		res.render('movie', { movie: data, countries: countries });
-	} catch (err) {
-		throw new Error(err);
-	}
+router.get("/mov/one/:movid", async (req, res) => {
+  try {
+    const data = await moviesModel.findOne({ _id: req.params.movid }).lean();
+    const countries = await manager.getCountries();
+    res.render("movie", { movie: data, countries: countries });
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
-router.get('/mov/country', async (req, res) => {
-	console.log('by country');
-	let currentPage;
-	const { country, page } = req.query;
-	if (page) {
-		currentPage = +page;
-	} else {
-		currentPage = '1';
-	}
-	console.log(country, currentPage);
+router.get("/mov/country", async (req, res) => {
+  console.log("by country");
+  let currentPage;
+  const { country, page } = req.query;
+  if (page) {
+    currentPage = +page;
+  } else {
+    currentPage = "1";
+  }
+  console.log(country, currentPage);
 
-	try {
-		const byCountry = await moviesModel.paginate({ countries: country }, { limit: 8, lean: true, page: +currentPage });
-		res.render('movies', { movies: byCountry });
-	} catch (error) {
-		throw new Error(err);
-	}
+  try {
+    const byCountry = await moviesModel.paginate(
+      { countries: country },
+      { limit: 8, lean: true, page: +currentPage }
+    );
+    res.render("movies", { movies: byCountry });
+  } catch (error) {
+    throw new Error(err);
+  }
 });
 
-router.get('/delete/:toClean', async (req, res) => {
-	const toClean = req.params.toClean;
-	try {
-		console.log(toClean);
-		const moviesToDelete = await model.paginate({ toClean: { $exists: false } }, { limit: 20 });
-		res.render('movies', { movies: moviesToDelete });
-	} catch (err) {
-		throw new Error(err);
-	}
+router.get("/delete/:toClean", async (req, res) => {
+  const toClean = req.params.toClean;
+  try {
+    console.log(toClean);
+    const moviesToDelete = await model.paginate(
+      { toClean: { $exists: false } },
+      { limit: 20 }
+    );
+    res.render("movies", { movies: moviesToDelete });
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
 export default router;
