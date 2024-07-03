b5b4c684913836b9485e88a41140e1de001663fa
Oscar Frischmann <oscarfrisc@gmail.com>
2024-07-03 10:27:47 -0300

    scope github auth

--- mflix/src/auth/passport.strategies.js
+++ mflix/src/auth/passport.strategies.js
@@ -1,82 +1,78 @@
-import passport from "passport";
-import local from "passport-local";
-import gitHub from "passport-github2";
-import UsersManager from "../dao/manager.mdb.js";
-import { isValidPassword } from "../utils.js";
-import config from "../config.js";
+import passport from 'passport';
+import local from 'passport-local';
+import gitHub from 'passport-github2';
+import UsersManager from '../dao/manager.mdb.js';
+import { isValidPassword } from '../utils.js';
+import config from '../config.js';
 
 const localStrategy = local.Strategy;
 const gitHubStrategy = gitHub.Strategy;
 const manager = new UsersManager();
 
 const initAuthStrategies = () => {
-  passport.use(
-    "local",
-    new localStrategy(
-      { passReqToCallback: true, usernameField: "user" },
-      async (req, username, password, done) => {
-        try {
-          const foundUser = await manager.findUser({ user: username });
+	passport.use(
+		'local',
+		new localStrategy({ passReqToCallback: true, usernameField: 'user' }, async (req, username, password, done) => {
+			try {
+				const foundUser = await manager.findUser({ user: username });
 
-          if (foundUser && isValidPassword(password, foundUser.password)) {
-            const { password, ...filteredFoundUser } = foundUser;
-            return done(null, filteredFoundUser);
-          } else {
-            return done(null, false);
-          }
-        } catch (err) {
-          return done(err, false);
-        }
-      }
-    )
-  );
+				if (foundUser && isValidPassword(password, foundUser.password)) {
+					const { password, ...filteredFoundUser } = foundUser;
+					return done(null, filteredFoundUser);
+				} else {
+					return done(null, false);
+				}
+			} catch (err) {
+				return done(err, false);
+			}
+		})
+	);
 
-  passport.use(
-    "ghlogin",
-    new gitHubStrategy(
-      {
-        clientID: config.GITHUB_CLIENT_ID,
-        clientSecret: config.GITHUB_CLIENT_SECRET,
-        callbackURL: config.GITHUB_CALLBACK_URL,
-      },
-      async (req, accessToken, refreshToken, profile, done) => {
-        try {
-          const email = profile._json?.email || null;
-          console.log(profile);
-          if (email) {
-            const foundUser = await manager.findUser({ email: email });
+	passport.use(
+		'ghlogin',
+		new gitHubStrategy(
+			{
+				clientID: config.GITHUB_CLIENT_ID,
+				clientSecret: config.GITHUB_CLIENT_SECRET,
+				callbackURL: config.GITHUB_CALLBACK_URL,
+			},
+			async (req, accessToken, refreshToken, profile, done) => {
+				try {
+					console.log(profile);
+					const email = profile._json?.email || null;
+					// console.log(profile);
+					if (email) {
+						const foundUser = await manager.findUser({ email: email });
 
-            if (!foundUser) {
-              const user = {
-                user:
-                  profile._json.name.split(" ")[0] +
-                  profile._json.name.split(" ")[1],
-                // lastName: profile._json.name.split(" ")[1],
-                email: email,
-                password: "none",
-              };
+						if (!foundUser) {
+							const user = {
+								user: profile._json.name.split(' ')[0] + profile._json.name.split(' ')[1],
+								// lastName: profile._json.name.split(" ")[1],
+								email: email,
+								password: 'none',
+							};
 
-              const process = await manager.createUser(user);
+							const process = await manager.createUser(user);
 
-              return done(null, process);
-            } else {
-              return done(null, foundUser);
-            }
-          } else {
-            return done(new Error("Faltan datos de perfil"), null);
-          }
-        } catch (err) {
-          return done(err, false);
-        }
-      }
-    )
-  );
-  passport.serializeUser((user, done) => {
-    done(null, user);
-  });
-  passport.deserializeUser((user, done) => {
-    done(null, user);
-  });
+							return done(null, process);
+						} else {
+							return done(null, foundUser);
+						}
+					} else {
+						return done(new Error('Faltan datos de perfil'), null);
+					}
+				} catch (err) {
+					return done(err, false);
+				}
+			}
+		)
+	);
+	passport.serializeUser((user, done) => {
+		done(null, user);
+	});
+	passport.deserializeUser((user, done) => {
+		done(null, user);
+	});
 };
 
 export default initAuthStrategies;
--- mflix/src/routes/sessions.routes.js
+++ mflix/src/routes/sessions.routes.js
@@ -1,15 +1,10 @@
-import { Router } from "express";
-import passport from "passport";
+import { Router } from 'express';
+import passport from 'passport';
 
-import config from "../config.js";
-import Manager from "../dao/manager.mdb.js";
-import {
-  loginAuth,
-  isValidPassword,
-  createHash,
-  verifyRequiredBody,
-} from "../utils.js";
-import initAuthStrategies from "../auth/passport.strategies.js";
+import config from '../config.js';
+import Manager from '../dao/manager.mdb.js';
+import { loginAuth, isValidPassword, createHash, verifyRequiredBody } from '../utils.js';
+import initAuthStrategies from '../auth/passport.strategies.js';
 // import localStrategy from '../auth/passport.strategies.js';
 
 const router = Router();
@@ -17,121 +12,101 @@ const manager = new Manager();
 initAuthStrategies();
 
 //PASSPORT GITHUB
-router.get(
-  "/ghlogin",
-  passport.authenticate("ghlogin", { scope: ["user"] }),
-  async (req, res) => {}
-);
+router.get('/ghlogin', passport.authenticate('ghlogin', { scope: ['user:email'] }), async (req, res) => {});
 
 router.get(
-  "/sessions/ghlogincallback",
-  passport.authenticate("ghlogin", {
-    failureRedirect: `/?error=${encodeURI("Error al identificar con Github")}`,
-  }),
-  async (req, res) => {
-    try {
-      req.session.user = req.user; // req.user es inyectado AUTOMATICAMENTE por Passport al parsear el done()
-      req.session.save((err) => {
-        if (err)
-          return res
-            .status(500)
-            .send({ origin: config.SERVER, payload: null, error: err.message });
+	'/sessions/ghlogincallback',
+	passport.authenticate('ghlogin', {
+		failureRedirect: `/?error=${encodeURI('Error al identificar con Github')}`,
+	}),
+	async (req, res) => {
+		try {
+			req.session.user = req.user; // req.user es inyectado AUTOMATICAMENTE por Passport al parsear el done()
+			req.session.save((err) => {
+				if (err) return res.status(500).send({ origin: config.SERVER, payload: null, error: err.message });
 
-        res.redirect("/mov");
-      });
-    } catch (err) {
-      res
-        .status(500)
-        .send({ origin: config.SERVER, payload: null, error: err.message });
-    }
-  }
+				res.redirect('/mov');
+			});
+		} catch (err) {
+			res.status(500).send({ origin: config.SERVER, payload: null, error: err.message });
+		}
+	}
 );
 //PASSPORT LOCAL
 router.post(
-  "/pplogin",
-  verifyRequiredBody(["user", "password"]),
-  passport.authenticate("local", {
-    failureRedirect: `/?error=${encodeURI("Usuario o clave no válidos")}`,
-  }),
-  async (req, res) => {
-    try {
-      // Passport inyecta los datos del done en req.user
-      req.session.user = req.user;
-      req.session.save((err) => {
-        if (err)
-          return res
-            .status(500)
-            .send({ origin: config.SERVER, payload: null, error: err.message });
+	'/pplogin',
+	verifyRequiredBody(['user', 'password']),
+	passport.authenticate('local', {
+		failureRedirect: `/?error=${encodeURI('Usuario o clave no válidos')}`,
+	}),
+	async (req, res) => {
+		try {
+			// Passport inyecta los datos del done en req.user
+			req.session.user = req.user;
+			req.session.save((err) => {
+				if (err) return res.status(500).send({ origin: config.SERVER, payload: null, error: err.message });
 
-        res.redirect("/mov");
-      });
-    } catch (err) {
-      res
-        .status(500)
-        .send({ origin: config.SERVER, payload: null, error: err.message });
-    }
-  }
+				res.redirect('/mov');
+			});
+		} catch (err) {
+			res.status(500).send({ origin: config.SERVER, payload: null, error: err.message });
+		}
+	}
 );
 // fin PASSPORT LOCAL
 
-router.post(
-  "/api/login",
-  verifyRequiredBody(["user", "password"]),
-  async (req, res) => {
-    const { user, password } = req.body;
-    try {
-      const match = await manager.findUser({ user: user });
-      if (match && isValidPassword(password, match.password)) {
-        const { password, ...filteredUser } = match;
-        req.session.user = filteredUser;
-        req.session.save((err) => {
-          if (err)
-            return res.status(500).send({
-              origin: config.SERVER,
-              payload: null,
-              error: err.message,
-            });
-          res.redirect("/mov");
-        });
-      } else {
-        res.redirect(`/?error=true`);
-      }
-    } catch (err) {
-      throw new Error(err);
-    }
-  }
-);
-
-router.post("/api/register", async (req, res) => {
-  try {
-    const { user, password, email } = req.body;
-    await manager.createUser({
-      user: user,
-      password: createHash(password),
-      email: email,
-    });
-    res.redirect("/");
-  } catch (err) {
-    throw new Error(err);
-  }
+router.post('/api/login', verifyRequiredBody(['user', 'password']), async (req, res) => {
+	const { user, password } = req.body;
+	try {
+		const match = await manager.findUser({ user: user });
+		if (match && isValidPassword(password, match.password)) {
+			const { password, ...filteredUser } = match;
+			req.session.user = filteredUser;
+			req.session.save((err) => {
+				if (err)
+					return res.status(500).send({
+						origin: config.SERVER,
+						payload: null,
+						error: err.message,
+					});
+				res.redirect('/mov');
+			});
+		} else {
+			res.redirect(`/?error=true`);
+		}
+	} catch (err) {
+		throw new Error(err);
+	}
 });
 
-router.get("/logout", async (req, res) => {
-  try {
-    req.session.destroy((err) => {
-      if (err)
-        return res.status(500).send({
-          origin: config.SERVER,
-          payload: "Error al ejecutar logout",
-          error: err,
-        });
-      res.redirect("/");
-    });
-  } catch (err) {
-    res
-      .status(500)
-      .send({ origin: config.SERVER, payload: null, error: err.message });
-  }
+router.post('/api/register', async (req, res) => {
+	try {
+		const { user, password, email } = req.body;
+		await manager.createUser({
+			user: user,
+			password: createHash(password),
+			email: email,
+		});
+		res.redirect('/');
+	} catch (err) {
+		throw new Error(err);
+	}
+});
+
+router.get('/logout', async (req, res) => {
+	try {
+		req.session.destroy((err) => {
+			if (err)
+				return res.status(500).send({
+					origin: config.SERVER,
+					payload: 'Error al ejecutar logout',
+					error: err,
+				});
+			res.redirect('/');
+		});
+	} catch (err) {
+		res.status(500).send({ origin: config.SERVER, payload: null, error: err.message });
+	}
 });
 
 export default router;
